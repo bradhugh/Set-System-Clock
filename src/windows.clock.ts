68df4b24aa635fac781216d3af3e366ed4684aba
Brad Hughes <bradhugh@microsoft.com>
2019-03-11 16:19:35 -0400

    Implement runas/sudo for Windows

--- src/windows.clock.ts
+++ src/windows.clock.ts
@@ -2,10 +2,15 @@ import { IDateTimeControl } from './shared';
 
 import { exec } from 'child_process';
 import * as dateFormat from 'dateformat';
+import * as windosu from 'windosu';
 
 export default class WindowsDateTimeControl implements IDateTimeControl {
     public setDateTime(dateTime: Date, options: any) {
-        exec(`date ${dateFormat(dateTime, 'm/d/yyyy')}`);
-        exec(`time ${dateFormat(dateTime, 'HH:MM:ss')}`);
+        let sudo = options.useSudo ? 'sudo ' : '';
+
+        let performExec: (command: string) => void = sudo ? windosu.exec : exec;
+
+        performExec(`date ${dateFormat(dateTime, 'm/d/yyyy')}`);
+        performExec(`time ${dateFormat(dateTime, 'HH:MM:ss')}`);
     }
 }
